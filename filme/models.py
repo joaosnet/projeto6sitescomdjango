4e6a19cc6c88a987302f28d756f099247ef4ff7b
joaosnet <87316339+joaosnet@users.noreply.github.com>
2023-07-26 15:39:01 -0300

    V 0.0.1

--- filme/models.py
+++ filme/models.py
@@ -1,9 +1,31 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
-# Criar o Filme
+LISTA_CATEGORIAS = (
+    ("ANALISES", 'Analises'),
+    ("PROGRAMACAO", "Programação"),
+    ("APRESENTACAO", "Apresentação"),
+    ("OUTROS", "Outros"),
+)
 
+# Criar o Filme
+class Filme(models.Model):
+    # - thumb
+    thumb = models.ImageField(upload_to='thumb_filmes')
+    # - título
+    titulo = models.CharField(max_length=100)
+    # - sinopse
+    sinopse = models.TextField(max_length=1000)
+    # - Categoria
+    categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
+    # - Quantidade de views
+    visualizacoes = models.IntegerField(default=0)
+    # - data de Criação
+    data_criacao = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.titulo
 # Criar os Epsoódios
 
 # Criar o Usuário
